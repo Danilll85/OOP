039962d7c64468096f6df9d902e37e9d34a6b552
Danilll85 <danilakuroedov85@gmail.com>
2024-03-01 23:25:47 +0300

    Server is running

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,21 @@ import express from "express";
 import path from "path";
 
 const PORT = 3000;
+const __dirname = path.resolve();
+const frontendPath = path.join(__dirname, "../", "Frontend");
+
+console.log(frontendPath);
 
 const app = express();
+
+app.set("views", frontendPath);
+app.set("view engine", "ejs");
+
+app.use(express.static(frontendPath));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
 app.listen(PORT, () => {
     console.log(`Server has been started on port ${PORT}`);
