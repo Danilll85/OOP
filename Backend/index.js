34f8b95c905750d08775e685557121f4709e137c
Danilll85 <danilakuroedov85@gmail.com>
2024-03-01 22:08:25 +0300

    chage json settings

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,10 @@
-import express from express
-import path from path;
+import express from "express";
+import path from "path";
 
 const PORT = 3000;
 
 const app = express();
 
+app.listen(PORT, () => {
+    console.log(`Server has been started on port ${PORT}`);
+});
