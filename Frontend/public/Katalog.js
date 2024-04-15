95dccf4015a061169b096dadaa8c1f40b3e8988a
Danilll85 <danilakuroedov85@gmail.com>
2024-04-15 23:35:00 +0300

    add frontend for rating

--- Frontend/public/Katalog.js
+++ Frontend/public/Katalog.js
@@ -109,3 +109,68 @@ buyButtons.forEach((button) => {
         });
     });
 });
+
+//-------------------------------------------------------------------
+//Р - рейтинг!
+
+const ratings = document.querySelectorAll(".rating");
+
+if (ratings.length > 0) {
+    initRatings();
+}
+
+function initRatings() {
+    let ratingActive, ratingValue;
+
+    for (let i = 0; i < ratings.length; i++) {
+        const rating = ratings[i];
+        initRating(rating);
+    }
+
+    function initRating(rating) {
+        initRatingVars(rating);
+        setRatingActiveWidth();
+
+        if (rating.classList.contains("rating_set")) {
+            setRating(rating);
+        }
+    }
+
+    function initRatingVars(rating) {
+        ratingActive = rating.querySelector(".rating_active");
+        ratingValue = rating.querySelector(".rating_value");
+    }
+
+    function setRatingActiveWidth(index = ratingValue.innerHTML) {
+        const ratingActiveWidth = index / 0.05;
+
+        ratingActive.style.width = `${ratingActiveWidth}%`;
+    }
+
+    function setRating(rating) {
+        const ratingItems = rating.querySelectorAll(".rating_item");
+        for (let index = 0; index < ratingItems.length; index++) {
+            const ratingItem = ratingItems[index];
+
+            ratingItem.addEventListener("mouseenter", function (e) {
+                initRatingVars(rating);
+                setRatingActiveWidth(ratingItem.value);
+            });
+
+            ratingItem.addEventListener("mouseleave", function (e) {
+                setRatingActiveWidth();
+            });
+
+            ratingItem.addEventListener("click", function (e) {
+                initRatingVars(rating);
+
+                //Отправка на сервер if да else нет
+
+                alert("Доработать отправку на сервер!");
+
+                ratingValue.innerHTML = index + 1;
+                setRatingActiveWidth();
+            });
+        }
+    }
+}
